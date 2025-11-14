4dad7fbb5296dda733759376062844cdcff9c881
Jiyun <93763950+Ljyyds@users.noreply.github.com>
2025-11-14 22:32:06 +0800

    Delete obj_function.m

--- obj_function.m
+++
@@ -1,7 +0,0 @@
-function Obj=obj_function(H,A,F,S,W,C,L_S,M,parameter)
-So = S-twodi23D(M,W);
-part1=0.5*parameter.gamma1*trace(So'*So);
-part2=0.5*parameter.gamma2*trace(C'*L_S*C);
-part3=trace(H' * A * F);
-Obj=part1+part2-part3;
-end
